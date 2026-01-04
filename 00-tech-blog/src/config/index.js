f419baf65c0d28576234fe5fb5e74d63920eb0f0
Badru <badru.siddique@gmail.com>
2026-01-04 21:27:08 +0530

    including appwrite env and DB setup with project

---
+++ 00-tech-blog/src/config/index.js
@@ -0,0 +1,9 @@
+const config = {
+    appwriteUrl: import.meta.env.VITE_APPWRITE_URL,
+    appwriteBucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID,
+    appwriteProjectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    appwriteDatabaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    appwriteCollectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+};
+
+export default config;
--- 00-tech-blog/src/main.jsx
+++ 00-tech-blog/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import config from './config';
+
+
+  console.log(`Tech Blog using appwrite with URL: ${config.appwriteUrl}`);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
